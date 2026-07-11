484f6114b30fac4572f6eff78d75b1db126af52c
hassansyrn-cmyk <hassan.syrn@gmail.com>
2026-07-11 18:55:03 +0400

    feat: make Privacy Policy link in Footer functional

--- src/game/Footer.tsx
+++ src/game/Footer.tsx
@@ -1,10 +1,14 @@
 import { VERSION } from './constants';
 
 export default function Footer() {
+  const openPrivacy = () => {
+    window.open('/privacy.html', '_blank');
+  };
+
   return (
     <footer className="game-footer">
       <span className="footer-links">
-        <a href="#" onClick={(e) => e.preventDefault()}>
+        <a href="#" onClick={(e) => { e.preventDefault(); openPrivacy(); }}>
           Privacy Policy
         </a>
         <a href="#" onClick={(e) => e.preventDefault()}>
